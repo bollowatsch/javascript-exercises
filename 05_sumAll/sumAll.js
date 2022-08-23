55aae27dd30af74cbbaad4b313b5ff898981731c
bollowatsch <stephan.bollauf@gmail.com>
2022-08-23 21:15:06 +0200

    solvejs-exercise-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0) {
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) sum += i;
+    return sum;
+  }
+  else return "ERROR";
 
 };
 
