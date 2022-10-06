0f0a0bc3914e16fe5b40a635cdfc8ec5b97d452d
bollowatsch <stephan.bollauf@gmail.com>
2022-10-06 15:00:07 +0200

    solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let product = 1;
+  for (let num of arr) {
+    product *= num;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  let pow = base;
+  while (exp > 1) {
+    pow *= base;
+    exp--;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let factorial = 1;
+  while (num > 1) {
+    factorial *= num;
+    num--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
