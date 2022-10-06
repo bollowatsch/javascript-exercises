53a1dc386581831df468d2e721a480a3d7903c9e
bollowatsch <stephan.bollauf@gmail.com>
2022-10-06 15:16:36 +0200

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  console.log(str);
+  str = str.toLowerCase();
+  str = str.replaceAll(/[^\w\s]/g, '');
+  console.log(str);
+  let j = str.length - 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[j]) return false;
+    j--;
+  }
+  return i === str.length - 1 && j === 0 ? true : false;
 };
 
 // Do not edit below this line
