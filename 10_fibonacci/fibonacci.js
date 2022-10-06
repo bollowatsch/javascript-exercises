dfef95e967576faaec7904c2a6de32d6d899d9de
bollowatsch <stephan.bollauf@gmail.com>
2022-10-07 01:00:13 +0200

    solved exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = parseInt(num);
+  if (num <= 0) return "OOPS";
+  let fib = [1, 1], i = 1;
+  while (i <= num - 1) {
+    fib.push(fib[i - 1] + fib[i]);
+    i++;
+  }
+  return fib[num - 1];
 };
 
 // Do not edit below this line
