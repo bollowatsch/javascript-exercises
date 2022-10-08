a8088592de00d6119b3d065d118c2f53b20e4957
bollowatsch <stephan.bollauf@gmail.com>
2022-10-08 10:48:30 +0200

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  peopleWithAge = people.map(p => {
+    return p.yearOfDeath ? { name: p.name, age: p.yearOfDeath - p.yearOfBirth } : { name: p.name, age: new Date().getFullYear() - p.yearOfBirth };
+  })
+  return peopleWithAge.reduce(maxAge);
 
 };
+
+function maxAge(personA, personB) {
+  return personA.age > personB.age ? personA : personB;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
